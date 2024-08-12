9ee8c05485c31b32b40cf7fe18b2084a0ac98cb9
guptashashankmca <165798455+guptashashankmca@users.noreply.github.com>
2024-08-12 11:41:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let student = {
+		name : 'Shashank',
+	};
+	Object.prototype.getkeys = function(){
+		return Object.keys(this);
+	};
+
+console.log(student.getkeys());
+

c37a6413bc509cca424a0e0a7db737da43ef4f3f
guptashashankmca <165798455+guptashashankmca@users.noreply.github.com>
2024-08-12 12:46:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-	// let student = {
-	// 	name : "Shashank",
-	// };
-	// // Object.prototype.getkeys = function(){
-	// // 	return Object.keys(this);
-	// // };
-	// let keys = Object.keys(student);
-
-	// console.log(keys);
-
-const myObj = { name: "John", age: 30, city: "New York" };
-const keys = myObj.getKeys();
-console.log(keys);  // ["name", "age", "city"]
+	let student = {
+		name : "Shashank",
+	};
+	Object.prototype.getkeys = function(){
+		return Object.keys(this);
+	};
+	console.log(getkeys());
 
